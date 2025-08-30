1d94f4d692a5f511c89654b30536625d8659b72b
speluffo <139576890+speluffo23@users.noreply.github.com>
2025-08-29 22:15:49 -0300

    error

--- mandatario.php
+++ mandatario.php
@@ -21,11 +21,11 @@
             <div class="menu-container">
                 <nav>
                     <ul>
-                        <li><a href="index.html">Inicio</a></li>
-                        <li><a href="administracion.html">Administración</a></li>
-                        <li><a href="mandatario.html">Mandatario</a></li>
-                        <li><a href="contact.html">Contactos</a></li>
-                        <li><a href="legales.html">Legales</a></li>
+                        <li><a href="index.php">Inicio</a></li>
+                        <li><a href="administracion.php">Administración</a></li>
+                        <li><a href="mandatario.php">Mandatario</a></li>
+                        <li><a href="contact.php">Contactos</a></li>
+                        <li><a href="legales.php">Legales</a></li>                                                                                                                                              
                     </ul>
                 </nav>
             </div>
